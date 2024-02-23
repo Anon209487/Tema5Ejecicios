64e1db96f3f55272c3020108c00ce1da5009a47b
Anon209487 <156091669+Anon209487@users.noreply.github.com>
2024-02-23 14:22:30 +0100

    actualizacion 2

---
+++ src/Ejercicios/Act4.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+public class Act4  { 
+  public static void main(String[] args) {
+//	  Crea un programa con una clase llamada Articulo con los siguientes atributos: nombre, precio (sin IVA), iva (siempre será 21) y cuantosQuedan (representa cuántos quedan en el almacén).
+//	  Añade un constructor con 3 parámetros que asigne valores a nombre, precio y cuantosQuedan. Dicho constructor deberá mostrar un mensaje de error si alguno de los valores nombre, precio o cuantosQuedan no son válidos.
+//	  Añade también los siguientes métodos:
+//	  Método para imprimir la información del artículo por pantalla.
+//	  Método getPVP que devuelva el precio de venta al público (PVP) con iva incluido. 
+//	  Método getPVPDescuento que devuelva el PVP con un descuento pasado como argumento. 
+//	  Método vender que actualiza los atributos del objeto tras vender una cantidad ‘x’ (si es posible). Devolverá true si ha sido posible (false en caso contrario). 
+//	  Método almacenar que actualiza los atributos del objeto tras almacenar una cantidad ‘x’.
+	}
+}
